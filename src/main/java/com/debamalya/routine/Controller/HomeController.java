4cae9c6a506510f760e713e1c9dc2f22f849bf6b
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 22:46:04 +0530

    cors

--- src/main/java/com/debamalya/routine/Controller/HomeController.java
+++ src/main/java/com/debamalya/routine/Controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @Slf4j
