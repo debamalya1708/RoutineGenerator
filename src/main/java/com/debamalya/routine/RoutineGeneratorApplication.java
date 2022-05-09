7b4fdc6279a5f7fb75cce19fc8f984a411b56849
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 21:36:21 +0530

    cors

--- src/main/java/com/debamalya/routine/RoutineGeneratorApplication.java
+++ src/main/java/com/debamalya/routine/RoutineGeneratorApplication.java
@@ -2,12 +2,25 @@ package com.debamalya.routine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RoutineGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoutineGeneratorApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/home/newRoutine").allowedOrigins("https://routinev1.herokuapp.com");
+			}
+		};
 	}
 
 }
