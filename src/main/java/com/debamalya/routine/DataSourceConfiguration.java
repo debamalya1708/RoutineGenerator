1d437e69256aa85ef84c35c72c554a39ef54bc41
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 21:20:43 +0530

    cors

--- src/main/java/com/debamalya/routine/DataSourceConfiguration.java
+++ src/main/java/com/debamalya/routine/DataSourceConfiguration.java
@@ -1,95 +1,96 @@
-//package com.debamalya.routine;
+package com.debamalya.routine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class DataSourceConfiguration.
+ */
+@Configuration
+public class DataSourceConfiguration {
+
+    /** The data source. */
+//	@Autowired
+//    private DataSource dataSource;
+
+    /**
+     * Primary jdbc template.
+     *
+     * @return the named parameter jdbc template
+     */
+//   @Bean
+//    public NamedParameterJdbcTemplate primaryJdbcTemplate() {
+//	   System.out.println("named paramter");
+//        return new NamedParameterJdbcTemplate(dataSource);
+//    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.javasampleapproach.restdata;
+//
+//import javax.sql.DataSource;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 ////import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 //
-//import javax.sql.DataSource;
-//
 //// TODO: Auto-generated Javadoc
-//
 ///**
 // * The Class DataSourceConfiguration.
 // */
-//@Configuration
+////@Configuration
 //public class DataSourceConfiguration {
 //
 //    /** The data source. */
-//	@Autowired
-//    private DataSource dataSource;
+//    //@Autowired
+//    //private DataSource dataSource;
 //
 //    /**
 //     * Primary jdbc template.
 //     *
 //     * @return the named parameter jdbc template
 //     */
-////   @Bean
-////    public NamedParameterJdbcTemplate primaryJdbcTemplate() {
-////	   System.out.println("named paramter");
-////        return new NamedParameterJdbcTemplate(dataSource);
-////    }
-//
+//   /* @Bean
+//    public NamedParameterJdbcTemplate primaryJdbcTemplate() {
+//        return new NamedParameterJdbcTemplate(dataSource);
+//    }
+//*/
 //}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-////package com.javasampleapproach.restdata;
-////
-////import javax.sql.DataSource;
-////
-////import org.springframework.beans.factory.annotation.Autowired;
-////import org.springframework.context.annotation.Bean;
-////import org.springframework.context.annotation.Configuration;
-//////import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-////
-////// TODO: Auto-generated Javadoc
-/////**
-//// * The Class DataSourceConfiguration.
-//// */
-//////@Configuration
-////public class DataSourceConfiguration {
-////
-////    /** The data source. */
-////    //@Autowired
-////    //private DataSource dataSource;
-////
-////    /**
-////     * Primary jdbc template.
-////     *
-////     * @return the named parameter jdbc template
-////     */
-////   /* @Bean
-////    public NamedParameterJdbcTemplate primaryJdbcTemplate() {
-////        return new NamedParameterJdbcTemplate(dataSource);
-////    }
-////*/
-////}
