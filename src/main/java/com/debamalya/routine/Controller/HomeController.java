2cae1d3f32ec16bb4937ac00782b040d22cb6c33
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 22:11:09 +0530

    cors

--- src/main/java/com/debamalya/routine/Controller/HomeController.java
+++ src/main/java/com/debamalya/routine/Controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-@CrossOrigin(origins = "https://koderbyte.com/routinue-generator")
+@CrossOrigin(origins = "https://koderbyte.com")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
