47e0ef4949fd2c23a7a35494e6e4616ed304d57a
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 21:30:08 +0530

    cors

--- src/main/java/com/debamalya/routine/Controller/HomeController.java
+++ src/main/java/com/debamalya/routine/Controller/HomeController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,6 +24,7 @@ public class HomeController {
     RoutineGenerate routineGenerate;
 
 
+    @CrossOrigin(origins = "https://routinev1.herokuapp.com")
     @PostMapping(value = "/newRoutine", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<ArrayList<ArrayList<ArrayList<Period>>>> generateRoutine(@RequestBody Routine routine) {
         System.out.println("getting the routine");
