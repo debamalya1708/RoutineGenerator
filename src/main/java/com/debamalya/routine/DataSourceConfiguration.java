cad7894c3fad19df716d0f4441a70c74dc20a11f
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-05-09 21:55:37 +0530

    cors

--- src/main/java/com/debamalya/routine/DataSourceConfiguration.java
+++ src/main/java/com/debamalya/routine/DataSourceConfiguration.java
@@ -38,7 +38,7 @@ public class DataSourceConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/home/newRoutine").allowedOrigins("http://127.0.0.1:5500/");
+                registry.addMapping("/home/newRoutine").allowedOrigins("https://koderbyte.com/routinue-generator/");
             }
         };
     }
